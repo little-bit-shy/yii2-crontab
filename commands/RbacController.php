081de129c9faad294d4dba0a8945fb02309bf9f2
little-bit-shy <xuguozhi2016>
2018-01-21 21:55:53 +0800

    优化rbac权限初始化脚本

--- commands/RbacController.php
+++ commands/RbacController.php
@@ -17,35 +17,25 @@ use yii\console\Controller;
  */
 class RbacController extends Controller
 {
+    /**
+     * yii rbac/init
+     */
     public function actionInit()
     {
         $auth = Yii::$app->getAuthManager();
+        // 删除所有已有权限相关数据
+        $auth->removeAll();
 
-        // 添加 "createPost" 权限
-        $createPost = $auth->createPermission('createPost');
-        $createPost->description = 'Create a post';
-        $auth->add($createPost);
+        // 添加 "v1/user-copy/create" 权限
+        $permission = $auth->createPermission('v1/user-copy/create');
+        $permission->description = 'v1/user-copy/create';
+        $auth->add($permission);
 
-        // 添加 "updatePost" 权限
-        $updatePost = $auth->createPermission('updatePost');
-        $updatePost->description = 'Update post';
-        $auth->add($updatePost);
+        // 添加 "ordinary_user" 角色并赋予 "createPost" 权限
+        $role = $auth->createRole('ordinaryUser');
+        $auth->add($role);
+        $auth->addChild($role, $permission);
 
-        // 添加 "author" 角色并赋予 "createPost" 权限
-        $author = $auth->createRole('author');
-        $auth->add($author);
-        $auth->addChild($author, $createPost);
-
-        // 添加 "admin" 角色并赋予 "updatePost"
-        // 和 "author" 权限
-        $admin = $auth->createRole('admin');
-        $auth->add($admin);
-        $auth->addChild($admin, $updatePost);
-        $auth->addChild($admin, $author);
-
-        // 为用户指派角色。其中 1 和 2 是由 IdentityInterface::getId() 返回的id （译者注：user表的id）
-        // 通常在你的 User 模型中实现这个函数。
-        $auth->assign($author, 2);
-        $auth->assign($admin, 1);
+        $auth->assign($role, 1);
     }
 }
