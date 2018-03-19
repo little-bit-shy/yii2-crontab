0569509871e0d074025f865fe44ed6cf6f62f876
xuguozhi <1533356676@qq.com>
2018-03-19 15:28:01 +0800

    debug

--- components/ArrayHelper.php
+++ components/ArrayHelper.php
@@ -5,18 +5,17 @@
  * Date: 2016/11/8
  * Time: 15:42
  * @author xuguoliang <1044748759@qq.com>
- * @desc 数组辅助
+ * @desc 树状结构
  */
 
 namespace app\components;
 
-use yii\helpers\BaseArrayHelper;
 use yii\helpers\StringHelper;
 
-class ArrayHelper extends BaseArrayHelper
+class ArrayHelper
 {
     /**
-     * 建立层次结构
+     * 建立树状结构
      */
     public static function menu($array, $key)
     {
@@ -24,54 +23,16 @@ class ArrayHelper extends BaseArrayHelper
         foreach ($array as $element) {
             $lastArray = &$result;
             $explode = StringHelper::explode($element[$key], '/');
-            if ($explode[1] === '*') {
-                continue;
+            $kk = '';
+            foreach ($explode as $k) {
+                if (!empty($k)) {
+                    $kk .= "/$k";
+                    $lastArray = &$lastArray[$kk];
+                }
             }
-            $parent = '/' . $explode[1] . '/*';
-            if ($element[$key] === $parent) {
-                $data = self::submenu($array, $element, $key, $parent);
-                $lastArray[] = $data;
-            }
+            $lastArray = $element;
             unset($lastArray);
         }
         return $result;
-    }
-
-    /**
-     * 获取子菜单
-     */
-    public static function submenu($array, $element, $key, $parent)
-    {
-        $lastResult = $element;
-        // 初步分配子数据
-        $explode = StringHelper::explode($parent, '/');
-        foreach ($array as $key => $value) {
-            $implode = implode($explode, '\/');
-            if (preg_match("/^{$implode}/", $value['name'])) {
-                if ($parent !== $value['name']) {
-                    $result = &$lastResult['children'][];
-                    $result = $value;
-                }
-            }
-            unset($result);
-        }
-
-        // 处理子菜单数据
-        foreach ($lastResult['children'] as $key => $value) {
-            // 判断是否有需要处理的子菜单数据
-            $subExplode = StringHelper::explode($value['name'], '/');
-            $end = end($subExplode);
-            if ($end !== '*') {
-                if (count($subExplode) !== count($explode)) {
-                    unset($lastResult['children'][$key]);
-                }
-            } else {
-//                $recursion = &$lastResult['children'][$key];
-//                $recursion = &$recursion['children'][];
-                self::submenu($array, $value, $key, $value['name']);
-            }
-        }
-
-        return $lastResult;
     }
 }
