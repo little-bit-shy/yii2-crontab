b14faec5d1e0ad90ea998eeb830d6ccbd609ff07
xuguozhi <1533356676@qq.com>
2018-07-04 17:47:55 +0800

    debug

--- modules/v1/config/db.php
+++ modules/v1/config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => \yii\db\Connection::className(),
     'dsn' => 'mysql:host=127.0.0.1;dbname=yii2restful',
     'username' => 'root',
-    'password' => '',
+    'password' => '123456',
     'charset' => 'utf8',
     'tablePrefix' => 'yii2_'
 ];
