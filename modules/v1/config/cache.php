5a61b29d631db1aa697f79f0c216ba1ad735821d
xuguozhi <1533356676@qq.com>
2018-11-30 10:30:21 +0800

    修改redis配置文件

--- modules/v1/config/cache.php
+++ modules/v1/config/cache.php
@@ -1,11 +1,23 @@
 <?php
 
-return [
-    'class' => \yii\redis\Cache::className(),
-    'redis' => [
-        'hostname' => '127.0.0.1',
-        'port' => 6379,
-        'database' => 0,
-    ],
-    'keyPrefix' => 'app_',
-];
+if (YII_DEBUG) {
+    return [
+        'class' => \yii\redis\Cache::className(),
+        'redis' => [
+            'hostname' => '127.0.0.1',
+            'port' => 6379,
+            'database' => 0,
+        ],
+        'keyPrefix' => 'app_',
+    ];
+} else {
+    return [
+        'class' => \yii\redis\Cache::className(),
+        'redis' => [
+            'hostname' => '127.0.0.1',
+            'port' => 6378,
+            'database' => 0,
+        ],
+        'keyPrefix' => 'app_',
+    ];
+}
--- modules/v1/config/redis.php
+++ modules/v1/config/redis.php
@@ -3,9 +3,19 @@
  * redis相关配置
  * 不建议redis版本低于 2.6.12
  */
-return [
-    'class' => \yii\redis\Connection::className(),
-    'hostname' => '127.0.0.1',
-    'port' => 6379,
-    'database' => 0,
-];
+
+if (YII_DEBUG) {
+    return [
+        'class' => \yii\redis\Connection::className(),
+        'hostname' => '127.0.0.1',
+        'port' => 6379,
+        'database' => 0,
+    ];
+} else {
+    return [
+        'class' => \yii\redis\Connection::className(),
+        'hostname' => '127.0.0.1',
+        'port' => 6378,
+        'database' => 0,
+    ];
+}
